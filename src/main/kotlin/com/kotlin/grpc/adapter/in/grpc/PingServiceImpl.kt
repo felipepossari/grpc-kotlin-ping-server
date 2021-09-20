c1d968c7d985f337429d708e2b999e7dae7c9436
Felipe Possari <fcpfelipe@hotmail.com>
2021-09-20 10:46:16 -0300

    Server stream call created

--- src/main/kotlin/com/kotlin/grpc/adapter/in/grpc/PingServiceImpl.kt
+++ src/main/kotlin/com/kotlin/grpc/adapter/in/grpc/PingServiceImpl.kt
@@ -4,6 +4,8 @@ import com.grpc.PingRequest
 import com.grpc.PingResponse
 import com.grpc.PingServiceGrpcKt
 import io.grpc.StatusException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -20,11 +22,23 @@ class PingServiceImpl() : PingServiceGrpcKt.PingServiceCoroutineImplBase() {
     override suspend fun ping(request: PingRequest): PingResponse = kotlin.runCatching {
         log.info("Unary - Ping request received. Address: {}", request.address)
 
-        return PingResponse.newBuilder().apply {
-            address = request.address
-            time = random.nextInt(1000)
-        }.build()
+        return buildResponse(request)
     }.onFailure { e -> throw e.toStatusException() }.getOrThrow()
+
+    private fun buildResponse(request: PingRequest) = PingResponse.newBuilder().apply {
+        address = request.address
+        time = random.nextInt(1000)
+    }.build()
+
+    override fun pingServerStream(request: PingRequest): Flow<PingResponse> = kotlin.runCatching {
+        flow {
+            log.info("ServerStream - Ping request received")
+            for (i in 1..5) {
+                emit(buildResponse(request))
+                kotlinx.coroutines.delay(500)
+            }
+        }
+    }.onFailure { e -> e.toStatusException() }.getOrThrow()
 }
 
 private fun Throwable.toStatusException(): StatusException =
